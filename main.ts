4b2b9728ff5e44454737a7b6e6350ecb62e3cfec
derammo <817160+derammo@users.noreply.github.com>
2023-02-15 20:28:12 -0500

    implemented simple block code command

--- main.ts
+++ main.ts
@@ -1,11 +1,18 @@
 import { ObsidianPluginBase } from "derobst/main";
 import { PluginServices } from "main/PluginServices";
 
-import { Settings, DEFAULT_SETTINGS, SettingTab } from "main/Settings";
+import { DEFAULT_SETTINGS, Settings, SettingTab } from "main/Settings";
+import { MarkdownPostProcessorContext } from "obsidian";
+import { FooRenderer } from "./src/foo/FooRenderer";
 
 export default class ObsidianPlugin extends ObsidianPluginBase<Settings> implements PluginServices {
 	async onload() {
 		await this.loadSettings(DEFAULT_SETTINGS);
+
+		// Register a code block handler for the "foo" language.  Note we should not use exclamation points or other punctuation of any kind in this identifier.
+		this.registerMarkdownCodeBlockProcessor("foo", (source: string, el: HTMLElement, ctx: MarkdownPostProcessorContext) => {
+			ctx.addChild(new FooRenderer(el, this, source));
+		});
 
         // TODO: Start up other components of your plugin here.
 
@@ -18,3 +25,4 @@ export default class ObsidianPlugin extends ObsidianPluginBase<Settings> impleme
 
     // TODO: Implement the core functionality of your plugin here, to be called via PluginServices.
 }
+
